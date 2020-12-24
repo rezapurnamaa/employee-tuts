ba91908110a9a10f406a9dcad2587191d60cd847
reza.arief <reza.arief@bonial.com>
2020-12-24 17:27:33 +0100

    add EmployeeForm component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
+    <employee-form/>
     <employee-table v-bind:employees="employees"/>
   </div>
 </template>
 
 <script>
-import EmployeeTable from '@/components/EmployeeTable.vue'
-
+import EmployeeTable from '@/components/EmployeeTable'
+import EmployeeForm from '@/components/EmployeeForm'
 export default {
   name: 'App',
   components: {
-    EmployeeTable
+    EmployeeTable,
+    EmployeeForm
   },
   data() {
     return {
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<form>
+			<label>Employee Name</label>
+			<input type="text" name="name">
+			<label>Employee Email</label>
+			<input type="text" name="email">
+			<button>Add Employee</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'employee-form',
+		data() {
+			return {
+				employee: {
+					name: '',
+					email: ''
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
