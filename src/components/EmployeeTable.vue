6cdab15e3f5480ab81b402174b51296249b93256
reza.arief <reza.arief@bonial.com>
2020-12-24 17:05:48 +0100

    add EmployeeTable, add css primitive UI

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<th>Employee Name</th>
+					<th>Employee Name</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Employee</td>
+					<td>emplyoee@email.com</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'employee-table'
+	}
+</script>
+<style scoped=""></style>
